573c8317b6f0df261edd7862ef266711a77d2d4b
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-04-25 01:18:38 +0530

    responsive logo and added superuser
    fixed left align logo bug

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
